ac630d232746fabc2dd8c9ced2e133fa15a07654
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-06-24 23:59:14 -0400

    OMG this angled shit I want to burn my eyes out.

--- assets/themes/vroomvroom/lib/inc/plugins.php
+++ assets/themes/vroomvroom/lib/inc/plugins.php
@@ -1,6 +1,7 @@
 <?php
-add_filter( 'gform_submit_button', 'msdlab_form_submit_button', 10, 2 );
+//add_filter( 'gform_submit_button', 'msdlab_form_submit_button', 10, 2 );
 function msdlab_form_submit_button($ret){
+    $ret = str_replace(' button', ' ', $ret);
     $ret = sprintf('<span class="skewit button"><span class="unskewit">%s</span></span>',$ret);
     return $ret;
 }
