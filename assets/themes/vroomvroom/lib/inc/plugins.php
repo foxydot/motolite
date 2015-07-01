68f6e4c64c693d1afa6f3f77bf976155b7f34314
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-07-01 13:32:20 -0400

    clean up function to place three upcoming events at the top of the calendar

--- assets/themes/vroomvroom/lib/inc/plugins.php
+++ assets/themes/vroomvroom/lib/inc/plugins.php
@@ -17,7 +17,7 @@ function msdlab_upcoming_events_list(){
     $events_label_plural = tribe_get_event_label_plural();
 
 //$posts = tribe_get_list_widget_events();
-$posts = tribe_get_events(
+$events= tribe_get_events(
             apply_filters(
                 'tribe_events_list_args', array(
                     'eventDisplay'   => 'list',
@@ -27,35 +27,35 @@ $posts = tribe_get_events(
         );
 
 // Check if any event posts are found.
-if ( $posts ) :
+if ( $events ) :
     $ret = '
     <div class="tribe-events-list">
     <ol class="hfeed vcalendar row">
     ';
         // Setup the post data for each event.
-        foreach ( $posts as $post ) :
-            setup_postdata( $post );
-            $start = tribe_get_start_date($post->ID,false,'m/d');
-            $end = tribe_get_end_date($post->ID,false,'m/d');
+        foreach ( $events as $event ) :
+            $start = tribe_get_start_date($event->ID,false,'m/d');
+            $end = tribe_get_end_date($event->ID,false,'m/d');
             $duration = $start == $end?$start:$start.' - '.$end;
-            $venue = tribe_get_venue();
+            $venue = tribe_get_venue($event->ID);
             $venue = $venue!=''?'- '.$venue:'';
+            $description = msdlab_get_excerpt($event->ID);
             $ret .= '
             <li class="tribe-events-list-widget-events col-md-4 col-sm-12 '.tribe_events_event_classes($post->ID,false).'">
                 <div class="date-label skewit">
                     <div class="date unskewit">
-                        '.tribe_get_start_date($post->ID,false,'m/d/y').'
+                        '.tribe_get_start_date($event->ID,false,'m/d/y').'
                     </div>
                 </div>
                 <div class="description">
-                    '.tribe_events_get_the_excerpt().'
+                    '.$description.'
                 </div>
                 <div class="meta">
                     <span class="duration">'.$duration.'</span>
                     <span class="venue">'.$venue.'</span>
                 </div>
                 <div class="event-title summary">
-                    <a href="'.esc_url( tribe_get_event_link() ).'" rel="bookmark">'.get_the_title().'</a>
+                    <a href="'.esc_url( tribe_get_event_link($event->ID) ).'" rel="bookmark">'.apply_filters('post_title',$event->post_title).'</a>
                 </div>
             </li>';
         endforeach;
